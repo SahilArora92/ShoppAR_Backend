096019d856049c92e6171d5711df89a50b5f8a4c
SahilArora92 <sahilarora092@gmail.com>
2018-02-28 22:55:05 -0800

    search functionality

--- NodeApp/routes/index.js
+++ NodeApp/routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose'); 
+var fs= require('fs');
+mongoose.connect('mongodb://shoppar:12345678@ds227168.mlab.com:27168/shoppardb',function(){})
+  .catch(err => { 
+  // mongoose connection error will be handled here
+  console.error('App starting error:', err.stack);
+  process.exit(1);
+});
+
+//load all files in models dir
+
+fs.readdirSync(__dirname+'/../models').forEach(function(filename){
+  if(~filename.indexOf('.js')) 
+  require(__dirname+'/../models/'+filename);
+});
+
+var Products=mongoose.model('products');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/searchProducts/:query', function(req, res, next) {
+  Products.find({$text: {$search: req.params.query}}, {score: {$meta: "textScore"}}).sort({score:{$meta:"textScore"}})
+  .then(function(doc){
+    //getting the first object
+    res.send({"products":doc});
+  });
 });
 
 module.exports = router;
